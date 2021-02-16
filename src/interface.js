c17547291991ce2bb7eb9e875f8a429d77beba4c
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-16 11:13:32 +0000

    uses an API to display the current temperature in London

--- src/interface.js
+++ src/interface.js
@@ -41,4 +41,8 @@ $(document).ready(function(){
     }
   }
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+
 });
